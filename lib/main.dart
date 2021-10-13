2a494a83d7bee962c6a7f84649908245ebc9404f
atmChurn <atm.churn.developer@gmail.com>
2021-10-12 22:45:05 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      // TODO: Add theme
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        // TODO: Style the title
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        // TODO: Style the body text
+        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+      ),
+    );
+  }
+}
