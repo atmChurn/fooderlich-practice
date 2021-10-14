45a20fd14916dda21dccabf9e9ee4d1001faa435
atmChurn <atm.churn.developer@gmail.com>
2021-10-13 21:28:56 -0500

    added multiple cards and style designs

---
+++ lib/author_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'circle_image.dart';
+
+class AuthorCard extends StatelessWidget {
+  final String authorName;
+  final String title;
+  final ImageProvider? imageProvider;
+
+  const AuthorCard({
+    Key? key,
+    required this.authorName,
+    required this.title,
+    this.imageProvider,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace return Container(...);
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
